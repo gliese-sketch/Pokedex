eee173c5eb3163fe8168b6530169dea0e1d77353
gliese-sketch <173663566+gliese-sketch@users.noreply.github.com>
2024-10-09 07:42:54 +0000

    add typesafety

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -8,13 +8,15 @@ import data from "./data.json"; // Data
 import PokemonCard from "./components/PokemonCard"; // Component
 
 // === DOM Targeting ===
-const inputEl = document.querySelector('input[type="text"]');
-const dataRow = document.querySelector("[data-row]");
+const inputEl = document.querySelector(
+  'input[type="text"]'
+) as HTMLInputElement;
+const dataRow = document.querySelector("[data-row]") as HTMLDivElement;
 
 renderPokemon(shuffle(data));
 
 // Give list, it will render them
-function renderPokemon(list) {
+function renderPokemon(list: object[]): void {
   dataRow.textContent = "";
 
   if (!list.length) {
@@ -34,11 +36,7 @@ function renderPokemon(list) {
 }
 
 // Will be invoked on search
-function handleSearch(input) {
-  // const filteredPokemon = data.filter((pokemonObj) => {
-  //   return pokemonObj.name.toLowerCase().includes(input);
-  // });
-
+function handleSearch(input: string): void {
   // Create fuse object
   const options = {
     keys: ["name", "abilities"],
@@ -47,7 +45,7 @@ function handleSearch(input) {
   const fuse = new Fuse(data, options);
 
   // Perform search
-  function performSearch() {
+  function performSearch(): object[] {
     if (!input) return data;
 
     const searched = fuse.search(input);
@@ -59,18 +57,20 @@ function handleSearch(input) {
   renderPokemon(filterdPokemon);
 }
 
-let debounceTimer;
+let debounceTimer: ReturnType<typeof setTimeout>;
 inputEl.addEventListener("input", (e) => {
   clearTimeout(debounceTimer);
 
+  const target = e.target as HTMLInputElement;
+
   debounceTimer = setTimeout(() => {
-    const currentInput = e.target.value.trim().toLowerCase();
+    const currentInput = target.value.trim().toLowerCase();
     handleSearch(currentInput);
   }, 400);
 });
 
 // Add / to active search
-document.addEventListener("keydown", (e) => {
+document.addEventListener("keydown", (e: KeyboardEvent) => {
   if (e.key === "/") {
     // Don't type
     e.preventDefault();
